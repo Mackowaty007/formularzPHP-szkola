baf1200b45c041987f54f9434ae9a6c5fdcce71f
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2022-10-05 16:14:25 +0200

    Add files via upload

--- form.php
+++ form.php
@@ -1,18 +1,81 @@
 <?php
-if(isset($_POST["submit"]))
-{
-    echo "Imie: {$_POST["imie"]}<br>Nazwisko: {$_POST["nazwisko"]}<br> Klasa: {$_POST["klasa"]}";
-    echo "<br>";
-    echo "Jesteś {$_POST["zawod"]}";
-    echo "<br>";
-    if($_POST["interest"] == "inne"){
-      echo "Interesujesz się {$_POST["inneZainteresowanie"]}";
-    }
-    else{
-      echo "Interesujesz się {$_POST["interest"]}";
-    }
-    echo "<br>";
-    echo "Rok: {$_POST["rok"]}";
+$dbServer = 'localhost';
+$dbUser = 'root';
+$dbPassword = '';
+$dbName = 'zadanieDomowe';
+
+$mysqli = new mysqli($dbServer,$dbUser,$dbPassword,$dbName);
+$mysqli->set_charset("utf8");
+if ( mysqli_connect_errno() ) {
+	echo 'Błąd bazy danych';
+}
+echo "baza danych sie poloczyla!<br>";
+
+$email = '';
+$password = '';
+$terms = '';
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$terms = $_POST['terms'];
+if ($terms == "on"){
+  $terms = 1;
+}
+else {
+  $terms = 0;
 }
 
+if ( ! $email ) {
+	echo 'Uzupełnij pole email';
+} elseif ( $email && ! filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+	echo 'Upewnij się, że adres email ma prawidłowy format';
+}
+if ( ! $password ) {
+	echo 'Uzupełnij pole hasło';
+} elseif ( $password && strlen($password) < 6 ) {
+	echo 'Hasło musi zawierać minimum 6 znaków';
+}
+if ( $terms  != 'on' ) {
+	echo 'Musisz zaakceptować regulamin';
+}
+/* wysylamy informacje do bazy danych */
+if ( ! $errorEmail && ! $errorPassword && ! $errorTerms ) {
+  echo $email;
+  echo "<br>";
+  echo $password;
+  echo "<br>";
+  echo $terms;
+  echo "<br>";
+  $query = $mysqli->query("INSERT INTO `informacje` (`ID`, `email`, `password`, `terms`) VALUES (NULL, '$email', '$password', '$terms');");
+  /*echo "works so far!";
+  $query = bind_param("ss",$email, $password);
+  echo "works so far????";
+  $query->execute();
+  $query->close();*/
+
+  $result = $mysqli->query("SELECT * FROM informacje");
+
+  while ( $wynik = mysqli_fetch_array($result) ) {
+   echo $wynik['email'];
+   echo "<br>";
+   echo $wynik['password'];
+   echo "<br>";
+   echo $wynik['terms'];
+   echo "<br><br>";
+ }
+}
+
+
+
 ?>
+
+<!DOCTYPE HTML>  
+<meta charset="utf-8">
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,69 +1,41 @@
-<?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
+<!DOCTYPE HTML>  
+<meta charset="utf-8">
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>  
+	<!-- Top -->
+	<div class="sk-top" id="top-header">
+		<div class="ui blue inverted segment sticky">
+			<div class="ui blue inverted menu">
+				<div class="ui container">
+					<a class="item">Start</a>
+				</div>
+			</div>
+		</div>		
+	</div>
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL";
-    }
-  }
+	<!--- form --->
+	<div class="main">
+	<h2>Karta ucznia:</h2>
+	<form method="post"  action="form.php">
+		<div class="dane">
+		email: <input type="text" name="email">
+		chaslo: <input type="text" name="password">
+		terms: <input type="checkbox" name="terms">
+		<input type="submit" name="submit" value="Submit">  
+		</div>
+	</form>
+	</div>
 
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
 
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
+	<!-- Stopka -->
+	<footer class="red sk-footer">
+		<div class="ui container">
+			<a href="" class="item">Kontakt</a>		
+		</div>
+	</footer>
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
+</body>
+</html>
