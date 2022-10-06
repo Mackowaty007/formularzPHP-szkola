3893f162d0ebf9c4575dfebcee81e53446ea582b
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2022-10-06 16:03:11 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,37 +5,37 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>  
-	<!-- Top -->
-	<div class="sk-top" id="top-header">
-		<div class="ui blue inverted segment sticky">
-			<div class="ui blue inverted menu">
-				<div class="ui container">
-					<a class="item">Start</a>
-				</div>
-			</div>
-		</div>		
+<!-- Top -->
+<div class="sk-top" id="top-header">
+  <a class="item">Start</a>	
+</div>
+
+<!--- form --->
+<div class="main">
+  <h2>Karta ucznia:</h2>
+  <form method="post"  action="form.php">
+    <div class="dane">
+      email: <input type="text" name="email">
+      chaslo: <input type="text" name="password">
+      terms: <input type="checkbox" name="terms">
+      <input type="submit" name="submit" value="Submit">  
+    </div>
+  </form>
+</div>
+
+
+<!-- Stopka -->
+<footer class="sk-footer">
+	<div class="ui container">
+		<a href="" class="item">Kontakt</a>		
 	</div>
-
-	<!--- form --->
-	<div class="main">
-	<h2>Karta ucznia:</h2>
-	<form method="post"  action="form.php">
-		<div class="dane">
-		email: <input type="text" name="email">
-		chaslo: <input type="text" name="password">
-		terms: <input type="checkbox" name="terms">
-		<input type="submit" name="submit" value="Submit">  
-		</div>
-	</form>
-	</div>
-
-
-	<!-- Stopka -->
-	<footer class="red sk-footer">
-		<div class="ui container">
-			<a href="" class="item">Kontakt</a>		
-		</div>
-	</footer>
+</footer>
+<script>
+		$(document).ready(function(){
+			$('.checkbox').checkbox();
+		});
+		$('.sticky').sticky();
+	</script>
 
 </body>
 </html>
