a2630b841a0ea16a9eb633153ddd906b229bb929
Mateusz <46724137+Mackowaty007@users.noreply.github.com>
2022-10-11 20:23:40 +0200

    Add files via upload

--- form.php
+++ form.php
@@ -25,7 +25,7 @@ if ( ! $password ) {
 	echo 'Hasło musi zawierać minimum 6 znaków';
   $errorPassword = "error password!!";
 }
-if ( $terms  != 'on' ) {
+if ( $terms  != 1 ) {
 	echo 'Musisz zaakceptować regulamin';
   $errorTerms = "error terms!!";
 }
@@ -38,13 +38,26 @@ if ( ! $errorEmail && ! $errorPassword && ! $errorTerms ) {
   echo $terms;
   echo "<br>";
   $query = $mysqli->query("INSERT INTO `informacje` (`ID`, `email`, `password`, `terms`) VALUES (NULL, '$email', '$password', '$terms');");
+  echo "it worked!";
   /*
   $query = bind_param("ss",$email, $password);
   echo "works so far????";
   $query->execute();
   $query->close();*/
+  $result = $mysqli->query("SELECT * FROM informacje");
+
+  while ( $wynik = mysqli_fetch_array($result) ) {
+  echo "<li>";
+    echo $wynik['email'];
+    echo "<br>";
+    echo $wynik['password'];
+    echo "<br>";
+    echo $wynik['terms'];
+    echo "</li>";
+    echo "<br><br>";
+  }
 }
 
-header('Location:index.php');
+//header('Location:index.php');
 
 ?>
